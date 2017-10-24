191707fe7741bcbd4e60c1f8c24bdb6fcf647b4d
Laurent Lemire <lemire.laurent824@hotmail.fr>
2017-10-23 21:33:21 -0400

    Ajout du script de la table formationsCompletes

---
+++ bd/Table_formationCompletes.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `formationCompletes` (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  employee_id int(11) NOT NULL,
+  formation_id int(11) NOT NULL,
+  lastTime_completed DATE NOT NULL,
+  comment VARCHAR(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (employee_id) REFERENCES employees(id),
+  FOREIGN KEY (formation_id) REFERENCES formations(id)
+ )ENGINE=MyISAM DEFAULT CHARSET=utf8;
